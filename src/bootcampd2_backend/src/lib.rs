e258afd0041245c39006c23b794933a6b20fa2cd
hmp561 <mkp.20006@gmail.com>
2024-06-26 17:37:22 +0200

    added way to edit wpis

--- src/bootcampd2_backend/src/lib.rs
+++ src/bootcampd2_backend/src/lib.rs
@@ -1,5 +1,7 @@
 use std::cell::RefCell;
 
+use ic_cdk::api::stable::StableMemory;
+
 thread_local!{
     static WPISY: RefCell<Vec<String>> = RefCell::default();
 }
@@ -27,5 +29,15 @@ fn odczytaj_wpisy() -> Vec<String>{
 fn usun_wpis(id_wpisu: usize){
     WPISY.with(|wpisy| {
        wpisy.borrow_mut().remove(id_wpisu)
+    });
+}
+
+#[ic_cdk::update]
+fn edytuj_wpis(id_wpisu: usize, nowy_wpis: String) {
+    WPISY.with(|wpisy| {
+        let mut binding = wpisy.borrow_mut();
+        let wpis = binding.get_mut(id_wpisu);
+        let stary_wpis = wpis.unwrap();
+        *stary_wpis = nowy_wpis;
     });
 }
